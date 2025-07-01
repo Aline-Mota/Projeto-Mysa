cca79989a9212a8c91889954c1605a09d67308f7
Aline Mota <alinegabriellycm@gmail.com>
2025-07-01 20:49:11 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle('dark');
+
+  // if(html.classList.contains('dark')) {
+  //   html.classList.remove('dark')
+  // } else {
+  //   html.classList.add('dark')
+  // }
+
+  //pegar a tag img
+  const img = document.querySelector('#profile img');
+  //substituir a imagem
+  if (html.classList.contains('dark')) {
+  //se tiver no dark mode, adicionar a imagem dark
+    img.setAttribute('src', './assets/gabi2.png');
+  } else {
+//se não tiver no dark mode, manter a imagem normal
+    img.setAttribute('src', './assets/gabi.png');
+  }
+}
